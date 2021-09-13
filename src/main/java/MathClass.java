9fe3b9e0e30a9e51b7d8ef187a8e4d8eb669e276
shupimumi <shupimumi@gmail.com>
2021-09-13 17:04:58 +0400

    Added tests

--- src/main/java/MathClass.java
+++ src/main/java/MathClass.java
@@ -1,7 +1,12 @@
+import methods.Sqrt;
+
 public class MathClass {
     public static void main(String[] args) {
-        double summ, divide, multiply, minus;
+        double summ, divide, multiply, minus, square_root;
         double firstEnteredNumber = 15.98, secondEnteredNuber = 87.98;
+        square_root = 15;
+        Sqrt sqrt = new Sqrt(square_root);
+
         summ = firstEnteredNumber+secondEnteredNuber;
         divide = firstEnteredNumber/secondEnteredNuber;
         multiply = firstEnteredNumber*secondEnteredNuber;
@@ -12,5 +17,6 @@ public class MathClass {
                 + "2. Divide is " + divide + "\n"
                 + "3. Multiply is " + multiply + "\n"
                 + "4. Difference is " + minus);
+        System.out.println("Square root is: " + sqrt.sqrt());
     }
 }
