70284c7c79e99d186dab1ccda6cf11e530a7ee73
shupimumi <shupimumi@gmail.com>
2021-09-13 16:01:25 +0400

    Initial commit

---
+++ src/main/java/MathClass.java
@@ -0,0 +1,16 @@
+public class MathClass {
+    public static void main(String[] args) {
+        double summ, divide, multiply, minus;
+        double firstEnteredNumber = 15.98, secondEnteredNuber = 87.98;
+        summ = firstEnteredNumber+secondEnteredNuber;
+        divide = firstEnteredNumber/secondEnteredNuber;
+        multiply = firstEnteredNumber*secondEnteredNuber;
+        minus = firstEnteredNumber-secondEnteredNuber;
+
+
+        System.out.println("1. Summ is " + summ + "\n"
+                + "2. Divide is " + divide + "\n"
+                + "3. Multiply is " + multiply + "\n"
+                + "4. Difference is " + minus);
+    }
+}
